2ca76aef83f0502d17a6112ff1fc7dcc15280ec2
Hossein <hossein.saafi@gmail.com>
2023-05-28 17:07:43 -0700

    a function that returns a string of 6 random alphanumeric characters

--- express_server.js
+++ express_server.js
@@ -40,6 +40,21 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString(){
+  characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    random_string = "";
+    for ( var i = 0; i <= 5; i++) {
+      var indx = Math.floor(62*Math.random());   // Generates random integer number between 0-61
+        random_string += characters[indx];
+    }
+    return random_string;
+}
